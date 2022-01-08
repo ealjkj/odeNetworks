11abc1d7c8538fc16f88c25a8f84c778185c5591
Jorge Ballote <ealjkj@gmail.com>
2022-01-08 03:24:17 -0600

    dataset added

--- myDataloaders.py
+++ myDataloaders.py
@@ -1,7 +1,8 @@
 import torch
-from torch.utils.data import Dataset, DataLoader
 import numpy as np 
-
+import albumentations
+from torch.utils.data import Dataset, DataLoader
+from PIL import Image
 
 class subDataset(Dataset):
     def __init__(self, dataset, alpha):
@@ -37,4 +38,45 @@ class NoisySet(Dataset):
         noise = torch.tensor(np.random.normal(mean, sigma, img_arr.shape), dtype=torch.float)
         return img_arr + noise
 
+class NaturalImageDataset(Dataset):
+    def __init__(self, path, labels, resize=84, tfms=None):
+        self.img_dim = resize
+        img_dim = self.img_dim
+        self.X = path
+        self.y = labels
+        # apply augmentations
+        if tfms == 0: # if validating
+            self.aug = albumentations.Compose([
+                albumentations.Resize(img_dim, img_dim, always_apply=True),
+                albumentations.Normalize(mean=[0.485, 0.456, 0.406],
+                          std=[0.229, 0.224, 0.225], always_apply=True)
+            ])
+        else: # if training
+            self.aug = albumentations.Compose([
+                albumentations.Resize(img_dim, img_dim, always_apply=True),
+                albumentations.Normalize(mean=[0.485, 0.456, 0.406],
+                          std=[0.229, 0.224, 0.225], always_apply=True)
+            ])    
+            self.aug = albumentations.Compose([
+                albumentations.Resize(img_dim, img_dim, always_apply=True),
+                albumentations.HorizontalFlip(p=0.8),
+                albumentations.ShiftScaleRotate(
+                    shift_limit=0.20,
+                    scale_limit=0.20,
+                    rotate_limit=0,
+                    p=0.8
+                ),
+                albumentations.Normalize(mean=[0.485, 0.456, 0.406],
+                          std=[0.229, 0.224, 0.225], always_apply=True)
+            ])
+
+    def __len__(self):
+        return (len(self.X))
+    
+    def __getitem__(self, i):
+        image = Image.open(self.X[i])
+        image = self.aug(image=np.array(image))['image']
+        image = np.transpose(image, (2, 0, 1)).astype(np.float32)
+        label = self.y[i]
+        return torch.tensor(image, dtype=torch.float), torch.tensor(label, dtype=torch.long)
 
